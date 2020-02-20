94b7770ee9cf259733728d66b8d7d136475004d3
Anara KD <rodnaya2303@gmail.com>
2020-02-19 19:11:54 -0600

    added

--- .github/deployments/terraform/charts/main.tf
+++ .github/deployments/terraform/charts/main.tf
@@ -1,7 +1,7 @@
 module "helm_deploy" {
   source                 = "git::https://github.com/fuchicorp/helm-deploy.git"
   deployment_name        = "example-deployment"
-  deployment_environment = "dev"
+  deployment_environment = "test"
   deployment_endpoint    = "anara2303-hello-world.fuchicorp.com"
   deployment_path        = "anara2303-hello-world"
 
