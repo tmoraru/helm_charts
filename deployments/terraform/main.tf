7f7351b88fad46c76ef40726d3d773fc3802d217
Murodbey <abdullayevmurod@gmail.com>
2020-02-19 19:25:44 -0600

    updated

---
+++ deployments/terraform/main.tf
@@ -0,0 +1,18 @@
+module "helm_deploy" {
+  source                 = "git::https://github.com/fuchicorp/helm-deploy.git"
+# Helm chart name 
+  deployment_name        = "murod-hello-world-deployment"
+# Namespace
+  deployment_environment = "test"
+ # endpoint to access application 
+  deployment_endpoint    = "murod-hello-world.fuchicorp.com"
+  # location of the chart
+  deployment_path        = "murod-hello-world"
+  
+  template_custom_vars = {
+
+    deployment_image = "nginx"
+    db_host_name     = "example-db.fuchicorp.com"
+    db_user_name     = "fuchicorp-example-user"
+  }
+}
