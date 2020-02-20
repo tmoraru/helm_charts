8730d9f7f74cb6717d866210041dff4743c097d4
Huseyin Akten <husakten@gmail.com>
2020-02-19 19:06:46 -0600

    created terraform module

---
+++ deployments/terraform/charts/main.tf
@@ -0,0 +1,7 @@
+module "helm_deploy" {
+  source                 = "git::https://github.com/fuchicorp/helm-deploy.git"
+  deployment_name        = "hakten-hello-world"
+  deployment_environment = "dev"
+  deployment_endpoint    = "hakten-hello-world.fuchicorp.com"
+  deployment_path        = "hakten-hello-world"
+}
