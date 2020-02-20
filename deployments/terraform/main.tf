a25ea906e86875f371a9f4cec7c2c6e1f802b164
Anastasia Clipco <anastasiaclipco@gmail.com>
2020-02-19 22:02:17 -0600

    edit

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -1,7 +1,10 @@
 module "helm_deploy" {
   source                 = "git::https://github.com/fuchicorp/helm-deploy.git"
   deployment_name        = "aclipco-hello-word"
-  deployment_environment = "tests"
+  deployment_environment = "test"
   deployment_endpoint    = "aclipco-hello-world.fuchicorp.com"
-  deployment_path        = "aclipco-hello-word"
+  deployment_path        = "aclipco-hello-world"
+    template_custom_vars = {
+    deployment_image = "nginx"
+ }
 }
