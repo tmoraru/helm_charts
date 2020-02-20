6fb57a1c7d5a5edffcc16db69ce4aab4d9395c44
Dil <dilhujaev@gmail.com>
2020-02-19 19:47:39 -0600

    added main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+module "helm_deploy" {
+  source                 = "git::https://github.com/fuchicorp/helm-deploy.git"
+ 
+ # helm chart name
+  deployment_name        = "dhujaev5-hello-world"
+  
+  #  namespace
+  deployment_environment = "test"
+  
+  # endpoint to access application
+  deployment_endpoint    = "dhujaev5-hello-world"
+  
+  # location of the chart
+  deployment_path        = "dhujaev5-hello-world"
+}
