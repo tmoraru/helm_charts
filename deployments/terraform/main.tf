f26c9e0bc4ef577ec960114a57076417d414b2fc
Mahsuda <maqsudat@yahoo.com>
2020-02-20 16:16:06 -0600

    deployment added

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -1,7 +1,16 @@
 module "helm_deploy" {
   source                 = "git::https://github.com/fuchicorp/helm-deploy.git"
-  deployment_name        = "mahsuda-hello-world"
-  deployment_environment = "dev"
-  deployment_endpoint    = "mahsuda-hello-world.fuchicorp.com"
-  deployment_path        = "mahsuda-hello-world"
+  # Helm chart name
+  deployment_name        = "Mahsuda-hello-world"
+  # Namespace
+  deployment_environment = "test"
+  # endpoint to access application
+  deployment_endpoint    = "Mahsuda-hello-world.fuchicorp.com"
+  # location of the chart
+  deployment_path        = "Mahsuda-hello-world"
+  template_custom_vars = {
+    deployment_image = "nginx"
+    db_host_name     = "example-db.fuchicorp.com"
+    db_user_name     = "fuchicorp-example-user"
+  }
 }
