f3af228e19386dbef2ba9bb8ed3efffbae199216
natan1988 <natan88@mail.ru>
2020-02-20 19:17:32 -0600

    added variables

---
+++ deployments/terraform/main.tf
@@ -0,0 +1,13 @@
+module "helm_deploy" {
+  source                 = "git::https://github.com/fuchicorp/helm-deploy.git"
+  deployment_name        = "natan1988-hello-world"
+  deployment_environment = "test"
+  deployment_endpoint    = "natan1988-hello-world.fuchicorpcom"
+  deployment_path        = "natan1988-hello-world"
+
+  template_custom_vars = {
+   deployment_image = "nginx"
+   db_host_name     = "example-db.fuchicorp.com"
+   db_user_name     = "fuchicorp-example-user"
+  }
+} 
