ff31a600352edf79c6d9f7791e8750fabafc6fdf
vovatran1993 <vovatran1993@gmail.com>
2020-02-21 12:31:23 -0600

    fixed

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -1,13 +1,22 @@
 module "helm_deploy" {
   source                 = "git::https://github.com/fuchicorp/helm-deploy.git"
+
+  # Helm chart name 
   deployment_name        = "rootvovak-hello-world"
+
+  # Namespace
   deployment_environment = "test"
+
+  # endpoint to access application 
   deployment_endpoint    = "rootvovak-hello-world.fuchicorp.com"
+
+  # location of the chart 
   deployment_path        = "rootvovak-hello-world"
 
   template_custom_vars = {
+
     deployment_image = "nginx"
-    db_host_name = "example-db.fuchicorp.com"
-    db_user_name = "fuchicorp-example-user"
+    db_host_name     = "example-db.fuchicorp.com"
+    db_user_name     = "fuchicorp-example-user"
   }
 }
