f75bded1d4a59cce61659767b9d68181f72a5f11
azizasalieva95 <azika1695@gmail.com>
2020-02-21 12:53:30 -0600

    fixed

--- deployments/main.tf
+++
@@ -1,7 +0,0 @@
-module "helm_deploy" {
-  source                 = "git::https://github.com/fuchicorp/helm-deploy.git"
-  deployment_name        = "azizasalieva95-hello-world"
-  deployment_environment = "dev"
-  deployment_endpoint    = "azizasalieva95-hello-world.fuchicorp.com"
-  deployment_path        = "azizasalieva95-hello-world"
-}
--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -1,22 +1,7 @@
 module "helm_deploy" {
   source                 = "git::https://github.com/fuchicorp/helm-deploy.git"
-
-  # Helm chart name 
   deployment_name        = "azizasalieva95-hello-world"
-
-  # Namespace
-  deployment_environment = "test"
-
-  # endpoint to access application 
+  deployment_environment = "dev"
   deployment_endpoint    = "azizasalieva95-hello-world.fuchicorp.com"
-
-  # location of the chart 
   deployment_path        = "azizasalieva95-hello-world"
-
-  template_custom_vars = {
-
-    deployment_image = "nginx"
-    db_host_name     = "example-db.fuchicorp.com"
-    db_user_name     = "fuchicorp-example-user"
-  }
 }
