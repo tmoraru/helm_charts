ec8761c4afe9e88139e4a09120a04b9b634abaf8
enver0173 <--list>
2020-02-21 15:30:04 -0600

    v2. added some more

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -2,16 +2,16 @@ module "helm_deploy" {
   source                 = "git::https://github.com/fuchicorp/helm-deploy.git"
 
   # Helm chart name 
-  deployment_name        = "enver0173-hello-world"
+  deployment_name        = "enver-hello-world.v2"
 
   # Namespace
   deployment_environment = "test"
 
   # endpoint to access application 
-  deployment_endpoint    = "enver0173-hello-world.fuchicorp.com"
+  deployment_endpoint    = "enver-hello-world.v2.fuchicorp.com"
 
   # location of the chart 
-  deployment_path        = "enver0173-hello-world"
+  deployment_path        = "enver-hello-world.v2"
 
   template_custom_vars = {
 
