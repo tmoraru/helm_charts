0f33ae7d24749b21c54042ba5ced1494d510361b
azizasalieva95 <azika1695@gmail.com>
2020-02-22 11:36:31 -0600

    changed

--- deployments/terraform/variables.tf
+++ deployments/terraform/variables.tf
@@ -1,4 +1,3 @@
-variable "tiller_version" {
-  default     = "v2.11.0"
-  description = "Please provide version of the tiller."
+variable "deployment_name" {
+  default = "azizasalieva95-hello-world"
 }
