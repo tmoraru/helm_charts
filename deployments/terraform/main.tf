499d94ffc5e6275b776fe8fd867c73919b25e44c
tunji57 <tunji57@gmail.com>
2020-02-22 13:12:23 -0600

    fixed code

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -8,7 +8,7 @@ module "helm_deploy" {
   deployment_environment = "${var.deployment_environment}"
 
   # endpoint to access application 
-  deployment_endpoint    = "tunji57-hello-world.fuchicorp.com"
+  deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}"
 
   # location of the chart 
   deployment_path        = "tunji57-hello-world"
--- deployments/terraform/variables.tf
+++ deployments/terraform/variables.tf
@@ -5,3 +5,13 @@ variable "deployment_name" {
 variable "deployment_environment" {
   default = "dev"
 }
+
+variable "deployment_endpoint" {
+  type = "map"
+
+  default = {
+    dev = "dev.tunji57-hello-world.fuchicorp.com"
+    qa = "qa.tunji57-hello-world.fuchicorp.com"
+    prod = "prod.tunji57-hello-world.fuchicorp.com"
+  } 
+}
