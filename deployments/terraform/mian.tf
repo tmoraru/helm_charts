646ae22065025399602f16eac570bc36e3880b46
Mohammad Karimi <mkarimidevops@gmail.com>
2020-02-22 12:33:59 -0600

    push

--- deployments/terraform/mian.tf
+++ deployments/terraform/mian.tf
@@ -1,17 +1,22 @@
 module "helm_deploy" {
-  source                 = "git::https://github.com/fuchicorp/helm-deploy.git"
- 
- 
+  # source                 = "git::https://github.com/fuchicorp/helm-deploy.git"
+  # source = "/Users/fsadykov/Projects/fuchicorp-projects/terraform-helm-chart"
+  source  = "fuchicorp/chart/helm"
+
+  # Helm chart name 
   deployment_name        = "${var.deployment_name}"
-  deployment_environment = "test"
-  deployment_endpoint    = "mkarimi20-hello-world.fuchicorp.com"
-  deployment_path        = "mkarimi20-hello-world"
 
-  
+  # Namespace
+  deployment_environment = "${var.deployment_environment}"
 
+  # endpoint to access application 
+  deployment_endpoint    = "fsadykov-hello-world.fuchicorp.com"
 
+  # location of the chart 
+  deployment_path        = "fsadykov-hello-world"
 
   template_custom_vars = {
+
     deployment_image = "nginx"
     db_host_name     = "example-db.fuchicorp.com"
     db_user_name     = "fuchicorp-example-user"
