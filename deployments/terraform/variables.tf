00116832cd8e2dec62a997f2afe8fdf7cc41604d
ulan <umanasov24@gmail.com>
2020-02-22 13:12:43 -0600

    added a new

--- deployments/terraform/variables.tf
+++ deployments/terraform/variables.tf
@@ -1,7 +1,19 @@
+variable "tiller_version" {
+  default     = "v2.11.0"
+  description = "Please provide version of the tiller."
+}
 variable "deployment_name" {
   default = "Ulan-devops-hello-world"
 }
 
 variable "deployment_environment" {
   default = "dev"
+}
+variable "deployment_endpoint" {
+  type = "map"
+  default = {
+    dev = "dev.Ulan-devops-hello-world.fuchicorp.com"
+    qa = "qa.Ulan-devops-hello-world.fuchicorp.com"
+    prod = "prod.Ulan-devops-hello-world.fuchicorp.com"
+  }
 }
