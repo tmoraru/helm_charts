d1349a7d425e5813c8985e79bf2b5bd6d9058c84
Ali <alisevhp@gmail.com>
2020-02-22 12:46:27 -0600

    as

--- deployments/terraform/charts/alisahp-hello-world/main.tf
+++ deployments/terraform/charts/alisahp-hello-world/main.tf
@@ -1,18 +1,23 @@
 module "helm_deploy" {
-  source                 = "git::https://github.com/fuchicorp/helm-deploy.git"
   source  = "fuchicorp/chart/helm"
-
   # Helm chart name 
-  deployment_name        = "alisahp-hello-world"
-
+  deployment_name        = "${var.deployment_name}"
   # Namespace
-  deployment_environment = "test"
-
+  deployment_environment = "${var.deployment_environment}"
   # endpoint to access application 
   deployment_endpoint    = "alisahp-hello-world.fuchicorp.com"
-
   # location of the chart 
   deployment_path        = "alisahp-hello-world"
-
   template_custom_vars = {
+    deployment_image = "nginx"
+    db_host_name     = "example-db.fuchicorp.com"
+    db_user_name     = "fuchicorp-example-user"
+  }
+}
+
+
+
+
+
+
 
