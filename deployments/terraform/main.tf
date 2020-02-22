8cde40f298c51a01bb5aac4cd15d620a3daf6e50
muratto33 <muratkilic33@yahoo.com>
2020-02-21 20:39:09 -0600

    yes

---
+++ deployments/terraform/main.tf
@@ -0,0 +1,18 @@
+module "helm_deploy" {
+  source                 = "git::https://github.com/fuchicorp/helm-deploy.git"
+  deployment_name        = "murat-hello-world"
+  deployment_environment = "test"
+  deployment_endpoint    = "murat-hello-world.fuchicorp.com"
+  deployment_path        = "murat-hello-world"
+
+
+
+  template_custom_vars = {
+    deployment_image = "nginx"
+    db_host_name     = "example-db.fuchicorp.com"
+    db_user_name     = "fuchicorp-example-user"
+  }
+}
+
+
+
