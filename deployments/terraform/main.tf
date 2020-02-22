6e04caadd2327e06c7d7c99746ec9cea16c9c193
Anastasia Clipco <anastasiaclipco@gmail.com>
2020-02-22 11:45:42 -0600

    jenkins file added

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -1,6 +1,6 @@
 module "helm_deploy" {
   source                 = "git::https://github.com/fuchicorp/helm-deploy.git"
-  deployment_name        = "aclipco-hello-word"
+  deployment_name        = "${var.deployment_name}"
   deployment_environment = "test"
   deployment_endpoint    = "aclipco-hello-world.fuchicorp.com"
   deployment_path        = "aclipco-hello-world"
