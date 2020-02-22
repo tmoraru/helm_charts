c758d20c2664dbeeb44f5607286c400772399c66
csamatov96 <csamatov96@gmail.com>
2020-02-22 12:18:24 -0600

    added

--- deployments/terraform/variables.tf
+++ deployments/terraform/variables.tf
@@ -1,9 +1,8 @@
 variable "deployment_name" { #reference 
-  default = "csamatov96-hello-world"
+    default = "csamatov96-hello-world"
 }
 
 variable "deployment_environment" {
     default = "dev"
-  
 }
 
