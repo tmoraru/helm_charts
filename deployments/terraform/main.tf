6ef6790a8e4031306d662bf17c5b502f4a324668
Anastasia Clipco <anastasiaclipco@gmail.com>
2020-02-22 13:16:40 -0600

    edit

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -2,7 +2,7 @@ module "helm_deploy" {
   source  = "fuchicorp/chart/helm"
   deployment_name        = "${var.deployment_name}"
   deployment_environment = "${var.deployment_environment}"
-  deployment_endpoint    = "${lookup(var.deployment_endpoint, "$var.deployment_environment}")}"
+  deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}"
   deployment_path        = "aclipco-hello-world"
 
   template_custom_vars = {
