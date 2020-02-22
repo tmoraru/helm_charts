676faa4a5d514a24ec2b49da6eb653229752fd0b
csamatov96 <csamatov96@gmail.com>
2020-02-22 13:24:21 -0600

    added

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -6,13 +6,12 @@ module "helm_deploy" {
   
   deployment_environment = "${var.deployment_environment}" #
 
-  deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}" #DNS
-  
+  deployment_endpoint    = "${lookup(var.deployment_endpoint, “${var.deployment_environment}“)}"
+
   deployment_path        = "csamatov96-hello-world" #path for helm chart on local 
   #
   template_custom_vars = {
+    
     deployment_image = "nginx"
-  #   db_host_name = "csamatov96-db.fuchicorp.com"
-  #   db_user_name = "fuchicorp-csamatov96-user"
    }
 }
