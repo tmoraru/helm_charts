0b5b6cda2c200f597feb3c7f081d18e8a855009a
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro-2.local>
2020-02-22 12:47:05 -0600

    added jenkinsfile

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -1,7 +1,7 @@
 module "helm_deploy" {
-  source                 = "git::https://github.com/fuchicorp/helm-deploy.git"
-  deployment_name        = "nurzhan-hello-world"
-  deployment_environment = "test"
+  source                 = "fuchicorp/chart/helm"
+  deployment_name        = "${var.deployment_name}"
+  deployment_environment = "${var.deployment_environment}"
   deployment_endpoint    = "nurzhan-hello-world.fuchicorp.com"
   deployment_path        = "nurzhan-hello-world"
 
---
+++ deployments/terraform/variables.tf
@@ -0,0 +1,7 @@
+variable "deployment_name" {
+  default = "nurzhan-hello-world"
+}
+
+variable "deployment_environment" {
+    default = "dev"
+}
