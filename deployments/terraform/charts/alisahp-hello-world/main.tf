dce17557768d5811e6d719ba8a161ea01c6e66a5
Ali <alisevhp@gmail.com>
2020-02-22 13:20:05 -0600

    as

--- deployments/terraform/charts/alisahp-hello-world/main.tf
+++ deployments/terraform/charts/alisahp-hello-world/main.tf
@@ -1,19 +1,25 @@
 module "helm_deploy" {
+  # source                 = "git::https://github.com/fuchicorp/helm-deploy.git"
+  # source = "/Users/fsadykov/Projects/fuchicorp-projects/terraform-helm-chart"
   source  = "fuchicorp/chart/helm"
+
   # Helm chart name 
   deployment_name        = "${var.deployment_name}"
+
   # Namespace
   deployment_environment = "${var.deployment_environment}"
+
   # endpoint to access application 
-  deployment_endpoint    = "$(lookup(var.deployment_endpoint, "${var.deployment_environment}"))"
+  deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}"
+
   # location of the chart 
-  deployment_path        = "alisahp-hello-world"
+  deployment_path        = "fsadykov-hello-world"
+
   template_custom_vars = {
+
     deployment_image = "nginx"
   }
 }
-
-
 
 
 
--- deployments/terraform/charts/alisahp-hello-world/variable.tf
+++ deployments/terraform/charts/alisahp-hello-world/variable.tf
@@ -1,17 +1,22 @@
+variable "tiller_version" {
+  default     = "v2.11.0"
+  description = "Please provide version of the tiller."
+}
+
 variable "deployment_name" {
   default = "alisahp-hello-world"
 }
 
-variable "tiller_version" {
-    default =
-  
+variable "deployment_environment" {
+  default = "prod"
 }
+
 
 variable "deployment_endpoint" {
   type = "map"
-  default = {
-      dev = "dev.alisahp-hello-world.fuchicorp.com"
-      qa = "qa.alisahp-hello-world.fuchicorp.com"
-      prod = "prod.alisahp-hello-world.fuchicorp.com"
+  default  = {
+    dealisahp
+    qa = "qa.alisahp-hello-world.fuchicorp.com"
+    prod = "alisahp-hello-world.fuchicorp.com"
   }
 }
