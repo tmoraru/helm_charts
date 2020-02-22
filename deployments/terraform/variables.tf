a0ec9ecba4618bf98b41fb13972819694423101e
vovatran1993 <vovatran1993@gmail.com>
2020-02-22 11:31:45 -0600

    tiller var added

--- deployments/terraform/variables.tf
+++ deployments/terraform/variables.tf
@@ -1,3 +1,7 @@
+variable "tiller_version" {
+  default     = "v2.11.0"
+  description = "Please provide version of the tiller."
+}
 variable "deployment_name" {
   default = "rootvovak-hello-world"
 }
