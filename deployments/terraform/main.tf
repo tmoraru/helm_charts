80089bee11192c308c7d9e33d9894dfbdc207281
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-02-22 13:12:23 -0600

    "

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -8,7 +8,7 @@ module "helm_deploy" {
   deployment_environment = "${var.deployment_environment}"
 
   # endpoint to access application 
-  deployment_endpoint = "${lookup(var.deployment_endpoint, "${var.deployment_environment)}"
+  deployment_endpoint = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}"
 
   deployment_path    = "tazagul-hello-world.fuchicorp.com"
 
