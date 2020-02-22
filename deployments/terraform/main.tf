53f2804f9a4841ef8676e8d7f4719ff91b2fa424
seraaboagye <sera.aboagye@gmail.com>
2020-02-22 13:06:48 -0600

    deleted extras

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -18,8 +18,6 @@ module "helm_deploy" {
   template_custom_vars = {
 
     deployment_image = "nginx"
-    db_host_name     = "example-db.fuchicorp.com"
-    db_user_name     = "fuchicorp-example-user"
   }
 }
 
--- deployments/terraform/variables.tf
+++ deployments/terraform/variables.tf
@@ -2,11 +2,17 @@ variable "tiller_version" {
   default     = "v2.11.0"
   description = "Please provide version of the tiller."
 }
-
 variable "deployment_name" {
   default = "sera-ab-hello-world"
 }
-
 variable "deployment_environment" {
   default = "dev"
+}
+variable "deployment_endpoint" {
+  type = "map"
+  default  = {
+    dev = "dev.sera-ab-hello-world.fuchicorp.com"
+    qa = "qa.sera-ab-hello-world.fuchicorp.com"
+    prod = "sera-ab-hello-world.fuchicorp.com"
+  }
 }
