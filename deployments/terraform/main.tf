cc16a9680626e84624e6a9af3ba033f5858d2665
enver0173 <--list>
2020-02-22 13:31:30 -0600

    added some interpolition

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -1,23 +1,15 @@
 module "helm_deploy" {
-  #source                 = "git::https://github.com/fuchicorp/helm-deploy.git"
-  souce ="fuchicorp/chart/helm"
-
-  # Helm chart name 
+  source                 = "fuchicorp/chart/helm"
+ 
+ 
   deployment_name        = "${var.deployment_name}"
-
-  # Namespace
   deployment_environment = "${var.deployment_environment}"
-
-  # endpoint to access application 
-  deployment_endpoint    = "enver0173-hello-world.fuchicorp.com"
-
-  # location of the chart 
+  deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}"
   deployment_path        = "enver0173-hello-world"
-
+  
   template_custom_vars = {
-
+    
     deployment_image = "nginx"
-    db_host_name     = "example-db.fuchicorp.com"
-    db_user_name     = "fuchicorp-example-user"
+  
   }
 }
--- deployments/terraform/variables.tf
+++ deployments/terraform/variables.tf
@@ -1,12 +1,16 @@
-variable "tiller_version" {
-  default     = "v2.11.0"
-  description = "Please provide version of the tiller."
-}
-
 variable "deployment_name" {
-    default = "enver0173-hello-world"
+  default = "enver0173-hello-world"
 }
 
 variable "deployment_environment" {
     default = "dev"
+}
+
+variable "deployment_endpoint" {
+  type = "map"
+  default = {
+    dev  = "dev.enver0173-hello-world.fuchicorp.com"
+    qa   = "qa.enver0173-hello-world.fuchicorp.com"
+    prod = "prod.enver0173-hello-world.fuchicorp.com"
+  }
 }
