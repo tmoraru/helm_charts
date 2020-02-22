542d403d8c0e8ad090e9ab99de8e9ee3bf09b48f
ulan <umanasov24@gmail.com>
2020-02-22 13:48:05 -0600

    added

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -2,7 +2,7 @@ module "helm_deploy" {
  # source                 = "git::https://github.com/fuchicorp/helm-deploy.git"
    source  = "fuchicorp/chart/helm"
   # Helm chart name
-  deployment_name        = ${var.deployment_name}
+  deployment_name        = "${var.deployment_name}"
   # Namespace
   deployment_environment = "${var.deployment_environment}"
   # endpoint to access application
