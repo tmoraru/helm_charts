ffae42826969d3e8492f8cc3f3c30b64da59fc69
azizasalieva95 <azika1695@gmail.com>
2020-02-22 11:07:15 -0600

    edited

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -1,7 +1,24 @@
 module "helm_deploy" {
-  source                 = "git::https://github.com/fuchicorp/helm-deploy.git"
+  # source                 = "git::https://github.com/fuchicorp/helm-deploy.git"
+  # source = "/Users/fsadykov/Projects/fuchicorp-projects/terraform-helm-chart"
+  source  = "fuchicorp/chart/helm"
+
+  # Helm chart name 
   deployment_name        = "azizasalieva95-hello-world"
-  deployment_environment = "dev"
+
+  # Namespace
+  deployment_environment = "test"
+
+  # endpoint to access application 
   deployment_endpoint    = "azizasalieva95-hello-world.fuchicorp.com"
+
+  # location of the chart 
   deployment_path        = "azizasalieva95-hello-world"
+
+  template_custom_vars = {
+
+    deployment_image = "nginx"
+    db_host_name     = "example-db.fuchicorp.com"
+    db_user_name     = "fuchicorp-example-user"
+  }
 }
---
+++ deployments/terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "tiller_version" {
+  default     = "v2.11.0"
+  description = "Please provide version of the tiller."
+}
