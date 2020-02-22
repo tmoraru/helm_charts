d4ec3db49cdccd9716c893ee090b3f33a68c21f4
seedoffd <seedoffd@gmail.com>
2020-02-22 13:47:12 -0600

    variable added

--- deployments/terraform/variable.tf
+++ deployments/terraform/variable.tf
@@ -8,11 +8,11 @@ variable "deployment_name" {
 variable "deployment_environment" {
   default = "dev"
 }
-variable "deployment_endpoint" {
-  type = "map"
-  default  = {
-    dev = "dev.seedoffd-hello-world.fuchicorp.com"
-    qa = "qa.seedoffd-hello-world.fuchicorp.com"
-    prod = "prod.seedoffd-hello-world.fuchicorp.com"
-  }
+variable "deployment_endpoint"{
+    type = "map"
+    default  = {
+        dev  = "dev.seedoffd-hello-world.fuchicorp.com"
+        qa   = "qa.seedoffd-hello-world.fuchicorp.com"
+        prod = "prod.seedoffd-hello-world.fuchicorp.com"
+    }
 }
