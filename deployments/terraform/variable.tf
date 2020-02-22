df1065757e6ce787870b87005930ae07ef28e33f
muratto33 <muratkilic33@yahoo.com>
2020-02-22 13:06:21 -0600

    add enviriment

--- deployments/terraform/variable.tf
+++ deployments/terraform/variable.tf
@@ -1,7 +1,18 @@
-variable "deployment_name" {
-  default = "fsadykov-hello-world"
-
-  variable “deployment_environment” {
-  default = “dev”
+variable "tiller_version" {
+  default     = "v2.11.0"
+  description = "Please provide version of the tiller."
 }
+variable "deployment_name" {
+  default = "murat-hello-world"
+}
+variable "deployment_environment" {
+  default = "dev"
+}
+variable "deployment_endpoint" {
+  type = "map"
+  default  = {
+    dev = "dev.murat-hello-world.fuchicorp.com"
+    qa = "qa.murat-hello-world.fuchicorp.com"
+    prod = "murat-hello-world.fuchicorp.com"
+  }
 }
