56699654caafccac9b8fa178e426eee31c0e4ba2
bkarakas <bkarakasu@gmail.com>
2020-02-22 12:35:24 -0600

    source changed

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -1,5 +1,6 @@
 module "helm_deploy" {
-  source                 = "git::https://github.com/fuchicorp/helm-deploy.git"
+  #source                 = "git::https://github.com/fuchicorp/helm-deploy.git"
+  source                 = "fuchicorp/chart/helm"
   deployment_name        = "${var.deployment_name}"
   deployment_environment = "${var.deployment_environment}"
   deployment_endpoint    = "bkarakas-hello-world.fuchicorp.com"
