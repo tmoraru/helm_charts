e60f4082a32540e740f96a4a90856ee63b4fc064
Huseyin Akten <husakten@gmail.com>
2020-03-08 11:56:45 -0500

    first try

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -1,18 +1,41 @@
-module "helm_deploy" {
-  source                 = "fuchicorp/chart/helm"
+# module "helm_deploy" {
+#   source                 = "fuchicorp/chart/helm"
  
  
-  deployment_name        = "${var.deployment_name}"
-  deployment_environment = "${var.deployment_environment}"
-  deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}"
-  deployment_path        = "hakten-hello-world"
+#   deployment_name        = "${var.deployment_name}"
+#   deployment_environment = "${var.deployment_environment}"
+#   deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}"
+#   deployment_path        = "hakten-hello-world"
 
   
 
-  template_custom_vars = {
-    deployment_image = "nginx"
+#   template_custom_vars = {
+#     deployment_image = "nginx"
+#   }
+# }
+
+data "template_file" "hakten-hello-world-template-file" {
+  template = "${file("charts/hakten-hello-world/values.yaml")}"
+
+  vars {
+    deployment_endpoint = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}"
   }
 }
 
 
+resource "local_file" "hakten_helm_chart_values" {
+  content  = "${trimspace(data.template_file.hakten-hello-world-template-file.rendered)}"
+  filename = "charts/.cache/hakten-hello-world-template-file.yaml"
+}
 
+## Deploy ingress controller
+resource "helm_release" "ingress_controller" {
+
+  values = [
+    "${data.template_file.hakten-hello-world-template-file.rendered}"
+  ]
+  deployment_environment = "${var.deployment_environment}"
+  deployment_name        = "${var.deployment_name}"
+  chart     = "./charts/hakten-hello-world"
+  namespace = "${var.deployment_environment}"
+}
