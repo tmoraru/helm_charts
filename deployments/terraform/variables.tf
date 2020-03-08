c5f6be8a0a1831a3eeeb97a168f6acc7bf5165c2
Huseyin Akten <husakten@gmail.com>
2020-03-07 23:27:51 -0600

    vars changed

--- deployments/terraform/variables.tf
+++ deployments/terraform/variables.tf
@@ -6,6 +6,11 @@ variable "deployment_environment" {
     default = "qa"
 }
 
+variable "deployment_image" {
+  default = "nginx"
+}
+
+
 variable "deployment_endpoint" {
   type = "map"
   default = {
