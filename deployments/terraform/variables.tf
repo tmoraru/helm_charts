372631471a78916eb3fe49f9fe24efccaaf1d519
Huseyin Akten <husakten@gmail.com>
2020-03-08 12:33:08 -0500

    yes

--- deployments/terraform/variables.tf
+++ deployments/terraform/variables.tf
@@ -6,17 +6,21 @@ variable "deployment_environment" {
     default = "dev"
 }
 
-variable "deployment_endpoint" {
-  type = "map"
-  default = {
-    dev  = "dev.tazagul.net"
-    qa   = "qa.tazagul.net"
-    prod = "prod.tazagul.net"
-  }
-}
+# variable "deployment_endpoint" {
+#   type = "map"
+#   default = {
+#     dev  = "dev.tazagul.net"
+#     qa   = "qa.tazagul.net"
+#     prod = "prod.tazagul.net"
+#   }
+# }
 
 variable "deployment_image" {
   default = "nginx"
   
+}
+
+variable "deployment_endpoint" {
+  default = "dev"
 }
 
