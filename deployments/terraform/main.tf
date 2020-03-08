e22b72b92e3b69593c8d55f018b7b6acdf78821a
Huseyin Akten <husakten@gmail.com>
2020-03-08 12:34:06 -0500

    yes

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -27,7 +27,7 @@
 # resource "local_file" "hakten_helm_chart_values" {
 #   content  = "${trimspace(data.template_file.hakten-hello-world-template-file.rendered)}"
 #   filename = "charts/.cache/hakten-hello-world-template-file.yaml"
-}
+# }
 
 ## Deploy ingress controller
 resource "helm_release" "hakten-hello-world" {
